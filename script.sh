cc95d6c7837f4251199e3fdcaef0ee7072628bef
root <root@jessie.raw>
2017-09-13 16:49:41 +0000

    Nosso Primeiro Projeto

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#-*- coding: utf8 -*- 
+clear
+echo
+echo 'Debian GNU/Linux 8 jessie tty1'
+echo
+echo -n 'jessie login: '
+read USER
+echo -n 'Password: '
+read -s PASS
+if [ $USER == "vagrant" ]; then
+
+echo 'Você passou'
+
+fi
